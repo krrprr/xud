9d624b3f1a90b83dc560005d63e77745de0780d3
Karl Ranna <karl@karlranna.com>
2018-11-07 20:12:39 +0200

    test(swaps): add boilerplate integration tests

---
+++ test/integration/Swap.spec.ts
@@ -0,0 +1,13 @@
+import chai, { expect } from 'chai';
+
+describe('Swaps', () => {
+
+  describe('acceptDeal', () => {
+
+    it('should error upon unsupported currency', async () => {
+      expect(true).to.equal(false);
+    });
+
+  });
+
+});
