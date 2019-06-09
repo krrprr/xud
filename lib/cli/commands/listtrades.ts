67bfc54245b42fefe66899624591d2730ead8274
Immanuel Segol <3ditds@gmail.com>
2019-06-09 15:52:09 +0300

    feat: add style to JSON output

--- lib/cli/commands/listtrades.ts
+++ lib/cli/commands/listtrades.ts
@@ -1,6 +1,30 @@
 import { Arguments } from 'yargs';
 import { callback, loadXudClient } from '../command';
-import { ListTradesRequest } from '../../proto/xudrpc_pb';
+import { ListTradesRequest, ListTradesResponse, Trade } from '../../proto/xudrpc_pb';
+import Table, { HorizontalTable } from 'cli-table3';
+import colors from 'colors/safe';
+
+const HEADERS = [
+  colors.blue('rhash'),
+  colors.blue('trade quantity'),
+  colors.blue('trading pair'),
+  colors.blue('order type'),
+];
+
+const displayTrades = (trades: ListTradesResponse.AsObject) => {
+  const table = new Table({ head: HEADERS }) as HorizontalTable;
+  trades.tradesList.forEach((trade: Trade.AsObject) => {
+    const type = trade.makerOrder ? 'maker' : 'taker';
+    table.push([
+      trade.rHash,
+      trade.quantity,
+      trade.pairId,
+      type,
+    ]);
+  });
+  console.log(colors.underline(colors.bold('\Trades:')));
+  console.log(table.toString());
+};
 
 export const command = 'listtrades [limit]';
 
@@ -17,5 +41,5 @@ export const builder = {
 export const handler = (argv: Arguments) => {
   const request = new ListTradesRequest();
   request.setLimit(argv.limit);
-  loadXudClient(argv).listTrades(request, callback(argv));
+  loadXudClient(argv).listTrades(request, callback(argv, displayTrades));
 };
